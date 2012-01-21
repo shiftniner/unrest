512c1437c02358752b84ff204586a3f18112218d
Shift Niner <shiftniner@pobox.com>
2012-01-20 23:27:54 -0500

    Added dungeon placer and test; changed moss stone to :moss-stone

--- src/mcmap/dungeon.clj
+++ src/mcmap/dungeon.clj
@@ -12,7 +12,7 @@
                          (< 0 y 6))
                       :air
                     (some #{0 20} [x y z])
-                      :mossy-cobblestone
+                      :moss-stone
                     (= [x y z] [10 1 10])
                       (mc-block :mob-spawner
                                 :mob "Zombie" :delay 0)
@@ -23,10 +23,48 @@
          (gen-mcmap-zone 6 6 6
             (fn [x y z]
               (cond (some #{0 5} [y z])
-                      :mossy-cobblestone
+                      :moss-stone
                     (= [x y z] [3 3 1])
-                      (mc-block :sign
-                                :text ["" "Hello," "Dungeon"])
+                      (mc-block :wall-sign
+                                :text ["" "Hello," "Dungeon"]
+                                :face :north)
                     :else
                     :air)))}])
+
+(defn maybe-box-lookup
+  ([box x y z]
+     (let [{x0 :x0, y0 :y0, z0 :z0, zone :zone} box
+           xd (zone-x-size zone)
+           yd (zone-y-size zone)
+           zd (zone-z-size zone)]
+       (when (and (>= x x0) (>= y y0) (>= z z0)
+                  (< x (+ x0 xd))
+                  (< y (+ y0 yd))
+                  (< z (+ z0 zd)))
+         (zone-lookup zone (- x x0) (- y y0) (- z z0))))))
+
+(defn place-dungeons
+  "Takes a zone and a seq of dungeon boxes, and returns the zone with
+the dungeons placed in it"
+  ;; Might be better to have this return a fn for gen-mcmap[-zone]
+  ([zone boxes]
+     (gen-mcmap-zone (zone-x-size zone)
+                     (zone-y-size zone)
+                     (zone-z-size zone)
+       (fn [x y z]
+         (or (some #(maybe-box-lookup % x y z)
+                   boxes)
+             (zone-lookup zone x y z))))))
+
+(defn dungeon-exercise-1
+  "Makes an area with just empty air and a dungeon"
+  ([x-chunks z-chunks dungeon]
+     (let [x-size (* x-chunks +chunk-side+)
+           z-size (* z-chunks +chunk-side+)
+           zone (gen-mcmap-zone x-size z-size (fn [x y z] :air))
+           zone (place-dungeons zone dungeon)
+           mcmap (gen-mcmap x-size z-size
+                            (fn [x y z]
+                              (zone-lookup zone x y z)))]
+       (mcmap-to-mcr-binary mcmap 0 0))))
 
