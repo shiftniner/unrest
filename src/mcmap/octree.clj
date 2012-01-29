cc2116e5f931f64b8e7645300333d7bc20b87518
Shift Niner <shiftniner@pobox.com>
2012-01-29 17:23:28 -0500

    octree bugfix; added oct-assoc-box and oct-any-intersecting?
    Bug was that I had been considering x1,y1,z1 as indices rather than
    spatial coordinates; 0 0 0 1 1 1 should consist of a single zone element,
    not eight.

--- src/mcmap/octree.clj
+++ src/mcmap/octree.clj
@@ -29,12 +29,17 @@ object added directly at that node"
 region, returns an octree with the object added at that cuboid
 region"
   ([oct o x0 y0 z0 x1 y1 z1]
-     (oct-assoc oct o x0 y0 z0 x1 y1 z1
-                (:max-dim oct)
-                (:min-dim oct)
-                (:x0 oct)
-                (:y0 oct)
-                (:z0 oct)))
+     (if (or (= x0 x1)
+             (= y0 y1)
+             (= z0 z1))
+       (throw (RuntimeException. (str "attempted to add a zero-size box"
+                                      " to an octree")))
+       (oct-assoc oct o x0 y0 z0 x1 y1 z1
+                  (:max-dim oct)
+                  (:min-dim oct)
+                  (:x0 oct)
+                  (:y0 oct)
+                  (:z0 oct))))
   ([oct o x0 y0 z0 x1 y1 z1 node-size min-dim node-x0 node-y0 node-z0]
      (let [oct (or oct
                    {:x0 node-x0, :y0 node-y0, :z0 node-z0,
@@ -50,12 +55,13 @@ region"
                x-midpoint (+ node-x0 half-size)
                y-midpoint (+ node-y0 half-size)
                z-midpoint (+ node-z0 half-size)
-               x-low  (<  x0 x-midpoint)
-               x-high (>= x1 x-midpoint)
-               y-low  (<  y0 y-midpoint)
-               y-high (>= y1 y-midpoint)
-               z-low  (<  z0 z-midpoint)
-               z-high (>= z1 z-midpoint)
+               x-low  (< x0 x-midpoint)
+               x-high (> x1 x-midpoint)
+               y-low  (< y0 y-midpoint)
+               y-high (> y1 y-midpoint)
+               z-low  (< z0 z-midpoint)
+               z-high (> z1 z-midpoint)
+               ;; There has got to be a better way ...
                nodes (if (and x-low y-low z-low)
                        (assoc nodes
                          0 (oct-assoc (nodes 0) o x0 y0 z0 x1 y1 z1
@@ -106,4 +112,48 @@ region"
                        nodes)]
            (assoc oct :nodes nodes))))))
 
+(defn oct-assoc-box
+  "Convenience function; takes an octree and a box and returns the
+octree with the box added"
+  ([oct box]
+     (let [ {x0 :x0, y0 :y0, z0 :z0,
+             xd :xd, yd :yd, zd :zd,} box]
+       (oct-assoc oct box x0 y0 z0
+                  (+ x0 xd)
+                  (+ y0 yd)
+                  (+ z0 zd)))))
 
+(defn- contents-intersecting?
+  ([oct x0 y0 z0 x1 y1 z1]
+     (let [contents (:contents oct)]
+       (some (fn [ {cx0 :x0, cy0 :y0, cz0 :z0,
+                    cx1 :x1, cy1 :y1, cz1 :z1} ]
+               (and (> x1 cx0)
+                    (> y1 cy0)
+                    (> z1 cz0)
+                    (< x0 cx1)
+                    (< y0 cy1)
+                    (< z0 cz1)))
+             contents))))
+
+(defn oct-any-intersecting?
+  "Given an octree and two coordinates defining a cuboid region,
+returns true if the octree contains anything that intersects the
+region"
+  ([oct x0 y0 z0 x1 y1 z1]
+     (when oct
+       (if (or (<= x1 (:x0 oct))
+               (<= y1 (:y0 oct))
+               (<= z1 (:z0 oct)))
+         nil
+         (let [size (:size oct)
+               oct-x1 (+ size (:x0 oct))
+               oct-y1 (+ size (:y0 oct))
+               oct-z1 (+ size (:z0 oct))]
+           (if (or (>= x0 oct-x1)
+                   (>= y0 oct-y1)
+                   (>= z0 oct-z1))
+             nil
+             (or (contents-intersecting? oct x0 y0 z0 x1 y1 z1)
+                 (some #(oct-any-intersecting? % x0 y0 z0 x1 y1 z1)
+                       (:nodes oct)))))))))
