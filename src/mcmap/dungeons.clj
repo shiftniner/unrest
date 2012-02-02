1b18b8f437fdfaf652d71ff5a94358771813eddb
Shift Niner <shiftniner@pobox.com>
2012-02-01 22:40:15 -0500

    Updated a few things in hello-dungeon based on revised design decisions
    Entrance and spawners need seeds.
    Things get stacked from bottom to top (I might change my mind on this,
    given the convention with htable that code layout matches map layout).
    Spawner box is taller to allow room for occasional tall stacks.

--- src/mcmap/dungeons.clj
+++ src/mcmap/dungeons.clj
@@ -9,13 +9,15 @@
 
 (defn hello-dungeon
   ([y-level seed]
-     (-> (stack (pad 19 17 19)
-                (htable [(pad 7 2 19)]
-                        [(pad 5 2 7) (spawners 5 2 5)]
+     (-> (stack (htable [(pad 1 2 7) (box (prize-chest))]
                         [(pad 6 2 19)]
-                        [(pad 1 2 7) (box (prize-chest))]))
+                        [(pad 5 2 7) (spawners 5 5 5 (reseed seed 1))]
+                        [(pad 7 2 19)])
+                (pad 19 14 19))
          (surround :nether-brick)
          (surround :bedrock)
-         (add-entrance [2 2 10]
-                       "Hello, Dungeon"))))
+         (surround :ground)
+         (add-entrance [3 2 2]
+                       "Hello, Dungeon"
+                       (reseed seed 2)))))
 
