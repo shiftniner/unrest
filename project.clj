ad2ec413f4bec7e4df9b97f8e7e27795e8bbd98f
Shift Niner <shiftniner@pobox.com>
2012-04-19 22:43:52 -0400

    Upgraded to Clojure 1.4.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mcmap "0.0.0"
   :description "Libraries for outputting Minecraft maps, and an example tool"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[swank-clojure "1.3.3"]
                      [midje "0.4.0"]]
   :jvm-opts ["-Xmx4g -XX:-OmitStackTraceInFastThrow"])
