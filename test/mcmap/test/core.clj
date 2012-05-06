1b04e6885470b3c5e4bb9927f83ed21ea39b18fd
Shift Niner <shiftniner@pobox.com>
2012-05-05 23:01:37 -0400

    Added tests -- all except the final integration test

--- test/mcmap/test/core.clj
+++ test/mcmap/test/core.clj
@@ -1,6 +1,204 @@
 (ns mcmap.test.core
-  (:use [mcmap.core])
-  (:use [clojure.test]))
+  (:use [mcmap core cavern util balance toolkit dungeons blocks]
+        [mcmap.dungeon build]
+        [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+;;; I don't want to break anyone's favorite seed.  Unlike some people.
+;;; Ahem.
+
+(let [one (java.math.BigInteger/ONE)
+      bitmask (.subtract (.shiftLeft one 256) one)
+      multiplier (java.math.BigInteger. "127")]
+  (defn checksum-seq
+    "Takes a seq of numbers and returns a 256-bit checksum for them"
+    ([ns]
+       (reduce (fn [^BigInteger a ^BigInteger b]
+                 (.and bitmask
+                       (.add b
+                             (.multiply a multiplier))))
+               one
+               (map #(if (instance? BigInteger %)
+                       %
+                       (BigInteger/valueOf (long %)))
+                    ns)))))
+
+(defn zone-checksum
+  "Takes a zone and returns a 256-bit checksum for it"
+  ([zone]
+     (checksum-seq
+      (for [x (range (zone-x-size zone))]
+        (checksum-seq
+         (for [z (range (zone-z-size zone))
+               y (range (zone-y-size zone))]
+           (hash (zone-lookup zone x y z))))))))
+
+(deftest checksums
+  (testing "Testing the underlying zone-checksum test infrastructure"
+    (is (= (checksum-seq [])
+           1))
+    (is (= (checksum-seq [0])
+           127))
+    (is (= (checksum-seq [0 0 0])
+           2048383))
+    (is (= (checksum-seq [1000])
+           1127))
+    (is (= (checksum-seq [-1000])
+           (bignum 1157920892373161954235709850086879078532699846656405
+                   64039457584007913129639063)))
+    (is (= (checksum-seq (range 1000))
+           (bignum 8858038687629214766623955830174043534271365942043770
+                   1600814724188529539976693)))
+    (is (= (zone-checksum (gen-mcmap-zone 1 1 1 (fn [x y z] 1)))
+           255))
+    (is (= (zone-checksum (gen-mcmap-zone 2 2 2 (fn [x y z] 1)))
+           33562804097))
+    (is (= (zone-checksum (gen-mcmap-zone 4 4 4 (fn [x y z] 1)))
+           9530968806614672378527701940318915854081))
+    (is (= (zone-checksum (gen-mcmap-zone 20 20 20 (fn [x y z] 1)))
+           (bignum 1110062776744555089975633303793695442599661758652158
+                   "09788527157691703697406721")))
+    (is (= (zone-checksum (gen-mcmap-zone 8 8 8 (fn [x y z] :a)))
+           (bignum 7031538355156702668427685792414433255295429105897687
+                   "0278235343144045588512257")))))
+
+(deftest caverns
+  (testing "Testing cavern generation"
+    (binding [*map-gen-version* 1]
+      (let [ [z sx sz]
+               (noprint (epic-cave-network 4 64 64 64 1 {}))]
+        (is (= (zone-checksum z)
+               (bignum 352104113010423610463483464374975471656521070936
+                       97051474295604563063687448593)))
+        (is (= sx 15.057165838693523))
+        (is (= sz 32.47947172579647)))
+      (let [ [z sx sz]
+               (noprint (epic-cave-network 4 128 49 128 2 {}))]
+        (is (= (zone-checksum z)
+               (bignum 553748111353760343377832565248239635722665942879
+                       65748152525096807614694222277)))
+        (is (= sx 68.67277716744078))
+        (is (= sz 33.92345662985129)))
+      (let [e (try (noprint (epic-cave-network 2 128 48 128 2 {}))
+                   (catch Exception e e))]
+        (is (instance? Exception e))))))
+
+(defn dungeon-checksum
+  "Takes a rendered dungeon and returns a checksum for its zones"
+  ([d]
+     (checksum-seq
+      (map zone-checksum
+           (map (fn [b] @(:zone b))
+                (rest d))))))
+
+(defn prize-seq-checksum
+  "Takes values from prize-chest-contents and returns a checksum"
+  ([ps]
+     (checksum-seq (map hash ps))))
+
+(deftest chests
+  (testing "Testing prize chests"
+    (binding [*balance-version* [1 2 5 :a]]
+      (is (= (dungeon-checksum (render-dungeon (prize-chest 2)
+                                               {:reward 100}))
+             1039209778))
+      (is (= (prize-seq-checksum (prize-chest-items {:reward 100}
+                                                    1))
+             106003260754663599673953220694531516397193010338))
+      (is (= (dungeon-checksum (render-dungeon (prize-chest 2)
+                                               {:reward 2000}))
+             1113979714))
+      (is (= (prize-seq-checksum (prize-chest-items {:reward 2000}
+                                                    1))
+             (bignum 11579208923731619542357098500865981914125008742520
+                     6001325321982907900346511447)))
+      (is (= (prize-seq-checksum (prize-chest-items {:reward 50000}
+                                                    15))
+             6330164902744085885507629764322235115771304428497351264)))))
+
+(deftest dungeons
+  (testing "Testing available dungeons"
+    (binding [*map-gen-version* 1]
+      (is (set= (dungeon-types)
+                [:uncommon :home :std :rare]))
+      (is (set= (get-dungeons :std)
+                [:hello-dungeon :spite-and-malice]))
+      (is (set= (get-dungeons :uncommon)
+                [:back-and-forth :back-and-forth-backwards
+                 :back-and-forth-ii]))
+      (is (set= (get-dungeons :rare)
+                [:stairs-and-flowers :a-hole-in-the-wall]))
+      (is (set= (get-dungeons :home)
+                [:life-in-a-glass-house]))))
+  (testing "Testing rendered dungeons"
+    (binding [*map-gen-version* 1
+              *balance-version* [1 2 5 :a]]
+      (is (= (dungeon-checksum (render-dungeon
+                                (get-dungeon :hello-dungeon 64 1)
+                                {:pain 0.3 :reward 1000}))
+             (bignum 67561080368486769601746767309499942476895672061687
+                     443655260839251659617966970)))
+      (is (= (dungeon-checksum (render-dungeon
+                                (get-dungeon :hello-dungeon 64 1)
+                                {:pain 1.0 :reward 1000}))
+             (bignum 10748729452598738305369691168819944509558283787650
+                     7649063421380424861144338420)))
+      (is (= (dungeon-checksum (render-dungeon
+                                (get-dungeon :hello-dungeon 64 1)
+                                {:pain 0.0 :reward 1000}))
+             (bignum 49578957278297525849507159472188405017222530697244
+                     970527634169695240248237351)))
+      (is (= (dungeon-checksum (render-dungeon
+                                (get-dungeon :spite-and-malice 64 1)
+                                {:pain 0.3 :reward 1000}))
+             (bignum 56085526336405932130891835176040005092844110004190
+                     474095930191873944926072997)))
+      (is (= (dungeon-checksum (render-dungeon
+                                (get-dungeon :spite-and-malice 10 1)
+                                {:pain 0.3 :reward 1000}))
+             (bignum 92574915678685089953188172990331810184953296687397
+                     254819725965137881943815121)))
+      (is (= (dungeon-checksum (render-dungeon
+                                (get-dungeon :back-and-forth 64 1)
+                                {:pain 0.3 :reward 1000}))
+             (bignum 28638488820246996265580367978870993015350346108780
+                     60330262562952514703645766)))
+      (is (= (dungeon-checksum (render-dungeon
+                                (get-dungeon :back-and-forth-backwards
+                                             64 1)
+                                {:pain 0.3 :reward 1000}))
+             (bignum 56757223853115810451443118480118015529950173643102
+                     6658796408089673608962756)))
+      (is (= (dungeon-checksum (render-dungeon
+                                (get-dungeon :back-and-forth-ii 64 1)
+                                {:pain 0.3 :reward 1000}))
+             (bignum 68220471466778382886957170807444111126491127120834
+                     374661005841526462839813733)))
+      (is (= (dungeon-checksum (render-dungeon
+                                (get-dungeon :stairs-and-flowers 41 1)
+                                {:pain 0.4 :reward 1000}))
+             (bignum 55434366082795534234964659168378461195225923430229
+                     63784060838209603331981105)))
+      (is (= (dungeon-checksum (render-dungeon
+                                (get-dungeon :stairs-and-flowers 40 1)
+                                {:pain 0.4 :reward 1000}))
+             (bignum 37530287168388820958155067867574863422580098489918
+                     166924954033464859688298092)))
+      (is (= (dungeon-checksum (render-dungeon
+                                (get-dungeon :a-hole-in-the-wall 64 1)
+                                {:pain 0.3 :reward 1000}))
+             (bignum 94943009034778704047507874466431577149029717867279
+                     694624415191563638756246416)))
+      (is (= (dungeon-checksum (render-dungeon
+                                (get-dungeon :life-in-a-glass-house
+                                             64 1 +wool-color-labels+)
+                                {:pain 0.3 :reward 1000}))
+             (bignum 88600566095165161654648427037754123071731850324372
+                     982754055045373658789731048))))))
+
+(deftest whole-map
+  (testing (str "Integration test of full record-quest map generation"
+                " (short of .mca binary formatting)")
+    
+    ))
+
+
