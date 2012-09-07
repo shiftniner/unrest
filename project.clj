638d02ad28a50192463740cdd730ae01583124b2
Shift Niner <shiftniner@pobox.com>
2012-09-06 20:22:20 -0400

    Bumped version to 1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject unrest "0.1.0"
+(defproject unrest "1.0.2"
   :description "Libraries for outputting Minecraft maps, and an example tool"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[swank-clojure "1.3.3"]
